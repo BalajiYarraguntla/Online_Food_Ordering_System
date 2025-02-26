39db64f9cdbe2ef8a6c33c7bdc73b14ca85ba991
BalajiYarraguntla <baluadvik22@gmail.com>
2025-02-26 10:26:04 +0530

    Initial Commit

---
+++ src/main/java/com/online_food_ordering_system/Menu/Service/MenuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.online_food_ordering_system.Menu.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online_food_ordering_system/Menu/Service/MenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_food_ordering_system.Menu.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
